90abeef9f3bdb9b286afdbcb0f2ce81eca59b3b4
B. Perach <barp12@gmail.com>
2015-08-22 14:36:53 +0300

    fixed takewhile iterator to compute predicate only once

--- lambda/takewhile_iterator.h
+++ lambda/takewhile_iterator.h
@@ -12,7 +12,8 @@ namespace lambda {
 		takewhile_iterator(_Iter&& iter, _Func&& func,
 			std::enable_if_t<std::is_convertible<Iter, _Iter>::value && std::is_convertible<Pred, _Func>::value>* = 0) :
 			m_iterator(std::forward<_Iter>(iter)),
-			m_pred(std::forward<_Func>(func))
+			m_pred(std::forward<_Func>(func)),
+			computed_at_end(false)
 		{
 		}
 
@@ -30,23 +31,25 @@ namespace lambda {
 		takewhile_iterator & operator++()
 		{
 			++m_iterator;
+			computed_at_end = false;
 			return *this;
 		}
 
 		bool operator==(const takewhile_iterator & other) const
 		{
-			return m_iterator == other.m_iterator || !m_pred(*m_iterator);
+			return m_iterator == other.m_iterator || (computed_at_end && at_end) || (!(computed_at_end = true) || (at_end = !m_pred(*m_iterator)));
 		}
 
 		bool operator!=(const takewhile_iterator & other) const
 		{
-			return m_iterator != other.m_iterator && m_pred(*m_iterator);
+			return !(*this == other);
 		}
 
 	private:
 		Iter m_iterator;
 		mutable Pred m_pred; // the predicate can change everytime we call it
 		bool at_end;
+		bool computed_at_end;
 	};
 
 	template <typename Iter, typename Pred>
